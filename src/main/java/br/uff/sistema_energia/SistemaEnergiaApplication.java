fca96325a0aa3aa9d3bcbb97d0aee33cc3fda7e4
Jhonatan.Silva <Jhonatan.Silva@fazenda.rj.gov.br>
2024-11-08 11:55:17 -0300

    First commit

---
+++ src/main/java/br/uff/sistema_energia/SistemaEnergiaApplication.java
@@ -0,0 +1,13 @@
+package br.uff.sistema_energia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEnergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uff/sistema_energia/SistemaEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uff.sistema_energia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
